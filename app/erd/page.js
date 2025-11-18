db6c5797ecd42d86cdcca5ebcb6dc271a8bd8a2e
Abdul Ghaffar <agscontact777@gmail.com>
2025-11-18 21:22:21 +0500

    Gave full height to iframe for erd

--- app/erd/page.js
+++ app/erd/page.js
@@ -1,26 +1,30 @@
 import React from "react";
 
 const Page = () => {
-  return (
-    <div
-      style={{
-        width: "100%",
-        height: "calc(100% - 72px)",
-        margin: "0",
-        position: "absolute",
-        backgroundColor: "white",
+    return (
+        <div
+            style={{
+                width: "100%",
+                height: "calc(100% - 72px)",
+                margin: "0",
+                position: "absolute",
+                backgroundColor: "white",
 
-      }}
-    >
-      <iframe
-        id="wz99k79R-7oW"
-        src="https://lucid.app/documents/embedded/2cda1547-20f6-4bf5-9556-8b86878dcde5"
-        title="Lucidchart ERD"
-        allowFullScreen
-        style={{ width: "640px", height: "480px", border: 0 }}
-      />
-    </div>
-  );
+            }}
+        >
+            <iframe
+                id="wz99k79R-7oW"
+                src="https://lucid.app/documents/embedded/2cda1547-20f6-4bf5-9556-8b86878dcde5"
+                title="Lucidchart ERD"
+                allowFullScreen
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    border: 0
+                }}
+            />
+        </div>
+    );
 };
 
 export default Page;
